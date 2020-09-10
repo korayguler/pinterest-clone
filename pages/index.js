174a88344af820f246a59eb9b923add1037a0189
korayguler <tr.koray.guler@gmail.com>
2020-09-11 00:58:01 +0300

    pin component position bug fixed

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,30 @@
-import React from 'react'
 import Layout from 'components/Layout'
 import Pin from 'components/Pin'
-import StackGrid from 'react-stack-grid'
+import StackGrid, { transitions, easings } from 'react-stack-grid'
 import DummyData from 'dummy'
 
-function Homepage() {
+const transition = transitions.scaleDown
+export default function Homepage() {
   return (
     <Layout>
       <StackGrid
-        className='stackGrid'
+        duration={400}
+        monitorImagesLoaded
         columnWidth={235}
         gutterWidth={15}
         gutterHeight={40}
-        duration={1000}
         itemComponent={'div'}
+        easing={easings.cubicOut}
+        appearDelay={60}
+        appear={transition.appear}
+        appeared={transition.appeared}
+        enter={transition.enter}
+        entered={transition.entered}
+        leaved={transition.leaved}
       >
-        {DummyData.map((e) => (
+        {DummyData.map((e, i) => (
           <Pin
+            key={i}
             className='pin'
             title='Test Pin'
             desc='Test pin for my pinterest clone'
@@ -28,4 +36,3 @@ function Homepage() {
     </Layout>
   )
 }
-export default Homepage
