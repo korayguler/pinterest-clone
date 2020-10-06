5e6ec30d41cd0a11877b80498062977e9eb8b36d
Koray Guler <tr.koray.guler@gmail.comgit config --global user.email tr.koray.guler@gmail.com>
2020-10-06 23:14:30 +0300

    json-server api created api/data.json for rest stream

--- components/header/index.js
+++ components/header/index.js
@@ -1,5 +1,6 @@
 //REACT
 import { useRouter } from 'next/router';
+import { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 //STYLES
 import style from './style.module.scss';
@@ -9,49 +10,109 @@ import Button from 'components/button';
 import MainButton from 'components/main-button';
 import CircleImage from 'components/circle-image';
 import IconButton from 'components/icon-button';
-//HELPERS
-import ClassNames from 'classnames';
+import ModalArea from 'components/modal-area';
 //ICONS
 import * as Icon from 'components/icons';
 export default function Header({ children }) {
   const router = useRouter();
 
+  const [accountsOption, accountsOptionSet] = useState(false);
+  const [notification, notificationSet] = useState(false);
+  const [messages, messagesSet] = useState(false);
+
+  const autoClose = () => {
+    if (accountsOption) {
+      notificationSet(false);
+      messagesSet(false);
+    }
+    if (notification) {
+      accountsOptionSet(false);
+      messagesSet(false);
+    }
+
+    if (messages) {
+      accountsOptionSet(false);
+      notificationSet(false);
+    }
+  };
+
+  const closeArea = () => {
+    accountsOptionSet(false);
+    notificationSet(false);
+    messagesSet(false);
+  };
+  useEffect(() => {
+    autoClose();
+  }, []);
   return (
-    <header className={style.header}>
-      <Head>
-        <link rel='icon' type='image/png' sizes='32x32' href='favicon.png' />
-      </Head>
-      <div className={style.navigationContainer}>
-        <IconButton href={'/'}>
-          <Icon.MainLogo width={24} height={24} color={'rgb(var(--c-p))'} />
-        </IconButton>
-        <MainButton href={'/'} selected={router.pathname == '/' ? true : false}>
-          Home
-        </MainButton>
-        <MainButton
-          href={'/following'}
-          selected={router.pathname == '/following' ? true : false}
-        >
-          Following
-        </MainButton>
-      </div>
-      <SearchBar />
-      <div className={style.profileButtonsContainer}>
-        <IconButton>
-          <Icon.Notification width={24} height={24} />
-        </IconButton>
-        <IconButton>
-          <Icon.Message width={24} height={24} />
-        </IconButton>
+    <div>
+      <header className={style.header}>
+        <Head>
+          <link rel='icon' type='image/png' sizes='32x32' href='favicon.png' />
+        </Head>
+        <div className={style.navigationContainer}>
+          <IconButton href={'/'}>
+            <Icon.MainLogo width={24} height={24} color={'rgb(var(--c-p))'} />
+          </IconButton>
+          <MainButton
+            href={'/'}
+            selected={router.pathname == '/' ? true : false}
+          >
+            Home
+          </MainButton>
+          <MainButton
+            href={'/following'}
+            selected={router.pathname == '/following' ? true : false}
+          >
+            Following
+          </MainButton>
+        </div>
+        <SearchBar />
+        <div className={style.profileButtonsContainer}>
+          <IconButton
+            className={style.notificationBtn}
+            onClick={() => notificationSet(!notification)}
+          >
+            <Icon.Notification width={24} height={24} />
 
-        <IconButton href={'/profile'}>
-          <CircleImage src='/image/profile.jpg' size={'24px'} />
-        </IconButton>
+            {notification && (
+              <div className={style.hideCloseArea} onClick={closeArea}>
+                <ModalArea className={style.notification}>
+                  NOTİFİCATİON
+                </ModalArea>
+              </div>
+            )}
+          </IconButton>
+          <IconButton
+            className={style.messagesBtn}
+            onClick={() => messagesSet(!messages)}
+          >
+            <Icon.Message width={24} height={24} />
+            {messages && (
+              <div className={style.hideCloseArea} onClick={closeArea}>
+                <ModalArea className={style.messages}>MESSAGES</ModalArea>
+              </div>
+            )}
+          </IconButton>
 
-        <IconButton className={style.miniButton}>
-          <Icon.Down width={12} height={12} />
-        </IconButton>
-      </div>
-    </header>
+          <IconButton href={'/profile'}>
+            <CircleImage src='/image/profile.jpg' size={'24px'} />
+          </IconButton>
+
+          <IconButton
+            className={style.accountsOptionBtn}
+            className={style.miniButton}
+            onClick={() => accountsOptionSet(!accountsOption)}
+          >
+            <Icon.Down width={12} height={12} />
+            {accountsOption && (
+              <div className={style.hideCloseArea} onClick={closeArea}>
+                <ModalArea className={style.accountsOption}>modal</ModalArea>
+              </div>
+            )}
+          </IconButton>
+        </div>
+      </header>
+    </div>
   );
 }
